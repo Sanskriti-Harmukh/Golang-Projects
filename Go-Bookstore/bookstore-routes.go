1e87f2cf273cdca1234b2035ea21be4d409db1df
Sanskriti Harmukh <74777863+SanskritiHarmukh@users.noreply.github.com>
2022-05-18 18:24:14 +0530

    Add files via upload

---
+++ Go-Bookstore/bookstore-routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/SanskritiHarmukh/Golang-Projects/tree/main/Go-Bookstore/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookId).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
